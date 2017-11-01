28faa7173d1cb8723bfb0cbb7757d5b029c2ab29
Raimo Johanson <raimo.johanson@gmail.com>
2017-11-01 13:02:00 +0000

    update

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var server = http.createServer(app);
-//process.env.salt = 'ahoy'; //tee selgeks endale https://www.twilio.com/blog/2017/08/working-with-environment-variables-in-node-js.html
-//https://www.bigcommerce.com/blog/pci-compliance/
-//ISKE 
+
+require('dotenv').load();
 
 const nunjucks = require('nunjucks');
 
@@ -17,10 +16,13 @@ app.engine('html', nunjucks.render);
 app.set('view engine', 'html');
 
 app.use(express.static('client'));
-app.use('/', express.static('fe'));
+app.use(express.static('fe'));
 
 // load application
 require('./src/app')(app);
+
+app.use('/', express.static('fe'));
+
 
 // start server
 server.listen(process.env.PORT || 8080, process.env.IP || "0.0.0.0", function() {
