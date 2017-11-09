5bf49821f8c659f521d82bfd2301cf723b8f8ee5
Raimo Johanson <raimo.johanson@gmail.com>
2017-11-09 10:57:09 +0000

    update

--- src/user_session.js
+++ src/user_session.js
@@ -1,17 +1,9 @@
 'use strict';
-
 let async = require('async');
 const ERR_MESSAGE = 'REALM LOADING FAILED';
-// PERMISSION and ROLES middleware
-// Define and create main user objects and data containers
+
 module.exports = function(app, req, res, next) {
     let UsersRealms = app.locals.model.users_realms;
-    let Realms = app.locals.model.realms;
-    /**
-     * optimize query
-     */
-    //_realm_ = { realm_id : ROLE}
-
     res.locals.user._realms_ = {};
     UsersRealms.select('*', { user_id: res.locals.user.id }).then(results => {
 
@@ -24,11 +16,7 @@ module.exports = function(app, req, res, next) {
             res.locals.user._rids_.push(realm.realm_id);
             cb();
         }, () => {
-
             next();
         });
-
-
-
     }); //UsersRealms
 };
