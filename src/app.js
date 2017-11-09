9071d0bcc9bc4e9355ba0a20728f81c26ff48dfd
Raimo Johanson <raimo.johanson@gmail.com>
2017-11-09 10:58:42 +0000

    update

--- src/app.js
+++ src/app.js
@@ -17,29 +17,6 @@ module.exports = function(app) {
   // =============================================================================
   require('./model/index')(app);
 
-  /*
-    app.use(function(req, res, next) {
-      let whitelist = ['https://nellie-tiiger.c9users.io'];
-      let origin = req.headers.origin;
-
-      if (_.includes(whitelist, origin))
-        res.setHeader('Access-Control-Allow-Origin', origin);
-
-
-      res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-      res.header('Access-Control-Allow-Credentials', true);
-
-      // dont set cookie in OPTIONS REST method
-      if (req.method.toLocaleLowerCase() == 'options')
-        return res.send();
-      else
-        next();
-    });
-    */
-  // configure app to use bodyParser()
-
-  // this will let us get the data from a POST
   var rawBodySaver = function(req, res, buffer, encoding) {
     if (buffer && buffer.length) req.rawBody = buffer.toString(encoding || 'utf8');
   };
